0001ec11bde2afaf26cff89641c77bb0a4657877
kalonjis <kalonjistephane@gmail.com>
2025-03-21 13:23:27 +0100

    adapting imports of authService function

--- src/app/core/auth/guards/super-admin.guard.ts
+++ src/app/core/auth/guards/super-admin.guard.ts
@@ -1,11 +1,11 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
-import {AuthService} from '../services/auth-service.service';
 import {map} from 'rxjs';
 import {UserRole} from '../../../data/models/user/user-role';
+import {provideAuthService} from '../services/auth-service.service';
 
 export const superAdminGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthService);
+  const authService = provideAuthService();
   const router = inject(Router);
 
   return authService.currentUser$.pipe(
