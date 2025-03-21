a75bfe6bacafa7aa20cd1bbb7cccd72888e9feb4
kalonjis <kalonjistephane@gmail.com>
2025-03-21 12:18:17 +0100

    added new providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// src/app/app.config.ts
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import {httpErrorInterceptor} from './core/http/http-error-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
+    provideHttpClient(
+      withInterceptors([
+        httpErrorInterceptor
+      ])
+    ),
+    provideAnimations()
+  ]
 };
