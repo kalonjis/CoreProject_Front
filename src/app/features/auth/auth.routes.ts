6841985b28fa753d6fbe9eb2aa4399acdfc394a4
kalonjis <kalonjistephane@gmail.com>
2025-03-21 14:36:53 +0100

    activated login route

---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,32 @@
+import {Routes} from '@angular/router';
+
+export const AUTH_ROUTES: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login/login.component').then(m => m.LoginComponent)
+  },
+  /*{
+    path: 'signup',
+    loadComponent: () => import('./signup/signup.component').then(m => m.SignupComponent)
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () => import('./forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
+  },
+  {
+    path: 'reset-password',
+    loadComponent: () => import('./reset-password/reset-password.component').then(m => m.ResetPasswordComponent)
+  },
+  {
+    path: 'account-confirmation',
+    loadComponent: () => import('./account-confirmation/account-confirmation.component').then(m => m.AccountConfirmationComponent)
+  },
+  {
+    path: 'email-confirmation',
+    loadComponent: () => import('./email-confirmation/email-confirmation.component').then(m => m.EmailConfirmationComponent)
+  },
+  {
+    path: 'device-confirmation',
+    loadComponent: () => import('./device-confirmation/device-confirmation.component').then(m => m.DeviceConfirmationComponent)
+  }*/
+];
