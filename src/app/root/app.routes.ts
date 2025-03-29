2a3d7b9122bcc6cf8d5fa3f3d9d21cd00782ba9a
kalonjis <kalonjistephane@gmail.com>
2025-03-29 16:31:36 +0100

    added "profile" path

--- src/app/root/app.routes.ts
+++ src/app/root/app.routes.ts
@@ -10,6 +10,11 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('../features/home/home.component').then(m => m.HomeComponent)
   },
+  {
+    path: 'profile',
+    loadComponent: () => import('../features/profile/profile.component')
+      .then(m => m.ProfileComponent)
+  },
 /*
   // Routes authentifiées standard
   {
