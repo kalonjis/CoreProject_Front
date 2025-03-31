85a70c176372e88d18e880643ae48419a60ae977
kalonjis <kalonjistephane@gmail.com>
2025-03-31 19:35:09 +0200

    added "path: 'account-confirmation"

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -14,6 +14,10 @@ export const AUTH_ROUTES: Routes = [
     path: 'signup',
     loadComponent: () => import('./signup/signup.component').then(m => m.SignupComponent)
   },
+  {
+    path: 'account-confirmation',
+    loadComponent: () => import('./account-confirmation/account-confirmation.component').then(m => m.AccountConfirmationComponent)
+  }
   /*
 
   {
