abcfb262517d6ef3f460b68bb869941aeaee23cd
kalonjis <kalonjistephane@gmail.com>
2025-05-10 13:02:05 +0200

    uncommented "canActivate: [() => adminGuard()]," for all route

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -4,29 +4,29 @@ import { adminGuard } from '../../core/auth/guards/admin.guard';
 export const ADMIN_ROUTES: Routes = [
   {
     path: '',
-    //canActivate: [() => adminGuard()],
+    canActivate: [() => adminGuard()],
     loadComponent: () => import('./dashboard/admin-dashboard.component')
       .then(m => m.AdminDashboardComponent)
   },
   {
     path: 'users',
-    //canActivate: [adminGuard],
+    canActivate: [adminGuard],
     loadComponent: () => import('./users/user-list/user-list.component').then(m => m.UserListComponent)
   },
   {
     path: 'users/new',
-    //canActivate: [adminGuard],
+    canActivate: [adminGuard],
     loadComponent: () => import('./users/user-register/user-register.component').then(m => m.UserRegisterComponent)
   },
   {
     path: 'users/:id',
-    //canActivate: [() => adminGuard()],
+    canActivate: [() => adminGuard()],
     loadComponent: () => import('./users/user-detail/user-detail.component')
       .then(m => m.UserDetailComponent)
   },
   {
     path: 'users/:id/devices',
-    //canActivate: [() => adminGuard()],
+    canActivate: [() => adminGuard()],
     loadComponent: () => import('../devices/device-list/device-list.component')
       .then(m => m.DeviceListComponent)
   }
