15f5a721c0d0691d843029a7afa678a9f7010d1b
kalonjis <kalonjistephane@gmail.com>
2026-03-03 11:55:06 +0100

    step 3 - ok

--- src/app/root/app.config.ts
+++ src/app/root/app.config.ts
@@ -1,5 +1,5 @@
 // app.config.ts
-import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
+import {ApplicationConfig, APP_INITIALIZER, LOCALE_ID} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -20,6 +20,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([authInterceptor])
     ),
+    { provide: LOCALE_ID, useValue: 'fr' },
     {
       provide: APP_INITIALIZER,
       useFactory: initializeAuth,
